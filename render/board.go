6cce135178d53a4a97ba2331f9ca3ad0fe73a131
Torben <torbensky@users.noreply.github.com>
2022-01-31 10:45:11 -0800

    add test coverage for board placeSnake

--- render/board.go
+++ render/board.go
@@ -12,7 +12,6 @@ const (
 	BoardSquareSnakeBody
 	BoardSquareSnakeHead
 	BoardSquareSnakeTail
-	BoardSquareDeadSnake
 	BoardSquareHazard
 )
 
--- render/board_internal_test.go
+++ render/board_internal_test.go
@@ -36,3 +36,79 @@ func TestBoard(t *testing.T) {
 	b.addHazard(&engine.Point{X: 3, Y: 0})
 	assert.Equal(t, BoardSquareHazard, b.getContents(3, 0)[1].Type, "(3,0) should ALSO have hazard content")
 }
+
+func TestPlaceSnake(t *testing.T) {
+	b := NewBoard(11, 11)
+
+	t.Log("Placing an alive snake")
+	s := engine.Snake{
+		// define properties that matter to the board
+		Color: "#3B194D",
+		Body: []engine.Point{
+			{X: 0, Y: 0}, // head
+			{X: 0, Y: 1},
+			{X: 1, Y: 1}, // tail
+		},
+		Head: "beluga",
+		Tail: "rattle",
+	}
+	b.placeSnake(s)
+
+	// HEAD
+	c := b.getContents(0, 0)
+	require.Len(t, c, 1, "there should only be a head here")
+	assert.Equal(t, BoardSquareSnakeHead, c[0].Type, "this should be a head")
+	assert.Equal(t, "down", c[0].Direction, "the head should be pointing down")
+	assert.Equal(t, "#3B194D", c[0].HexColor, "the head should have the snake colour")
+	assert.Equal(t, "beluga", c[0].SnakeType, "the head should be customised")
+
+	// BODY
+	c = b.getContents(0, 1)
+	require.Len(t, c, 1, "there should only be a body here")
+	assert.Equal(t, BoardSquareSnakeBody, c[0].Type, "this should be a body")
+	assert.Equal(t, "#3B194D", c[0].HexColor, "the body should have the snake colour")
+	assert.Equal(t, "", c[0].SnakeType, "the body should not have a customization")
+
+	// TAIL
+	c = b.getContents(1, 1)
+	require.Len(t, c, 1, "there should only be a tail here")
+	assert.Equal(t, BoardSquareSnakeTail, c[0].Type, "this should be a tail")
+	assert.Equal(t, "right", c[0].Direction, "the tail should be pointing right")
+	assert.Equal(t, "#3B194D", c[0].HexColor, "the tail should have the snake colour")
+	assert.Equal(t, "rattle", c[0].SnakeType, "the tail should be customised")
+
+	t.Log("Placing a dead snake")
+	s = engine.Snake{
+		Death: &engine.Death{Cause: "", Turn: 10},
+		// define properties that matter to the board
+		Color: "#FFFFFF",
+		Body: []engine.Point{
+			{X: 5, Y: 9}, // head
+			{X: 5, Y: 8},
+			{X: 4, Y: 8}, // tail
+		},
+	}
+	b.placeSnake(s)
+
+	c = b.getContents(5, 9)
+	require.Len(t, c, 1, "there should only be a head here")
+	assert.Equal(t, BoardSquareSnakeHead, c[0].Type, "this should be a head")
+	assert.Equal(t, "up", c[0].Direction, "the head should be pointing up")
+	assert.Equal(t, "#cdcdcd", c[0].HexColor, "the head should have the dead snake colour")
+	assert.Equal(t, "regular", c[0].SnakeType, "the head should be default")
+
+	// BODY
+	c = b.getContents(5, 8)
+	require.Len(t, c, 1, "there should only be a body here")
+	assert.Equal(t, BoardSquareSnakeBody, c[0].Type, "this should be a body")
+	assert.Equal(t, "#cdcdcd", c[0].HexColor, "the body should have the dead snake colour")
+	assert.Equal(t, "", c[0].SnakeType, "the body should not have a customization")
+
+	// TAIL
+	c = b.getContents(4, 8)
+	require.Len(t, c, 1, "there should only be a tail here")
+	assert.Equal(t, BoardSquareSnakeTail, c[0].Type, "this should be a tail")
+	assert.Equal(t, "left", c[0].Direction, "the tail should be pointing left")
+	assert.Equal(t, "#cdcdcd", c[0].HexColor, "the tail should have the dead snake colour")
+	assert.Equal(t, "regular", c[0].SnakeType, "the tail should be default")
+}
